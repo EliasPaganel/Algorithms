49be961137250481da206902ad7eb45be78d433e
shilyaev <shilyaev@proitr.ru>
2019-09-09 19:04:33 +0500

    Реализация MergeSort

--- src/main/java/sorting/MergeSort.java
+++ src/main/java/sorting/MergeSort.java
@@ -1,7 +1,26 @@
 package sorting;
 
-public class MergeSort<T extends Comparable<T>> implements Sort<T>{
+import java.util.function.Predicate;
+
+public class MergeSort<T extends Comparable<T>> implements Sort<T> {
     public void sorting(T[] mixedArray, SortType type) {
+        mixedArray = execute(mixedArray, 0, mixedArray.length - 1, SortType.getPredicate(type));
+    }
+
+    private T[] execute(T[] mixedArray, int firstIndex, int lastIndex, Predicate<Integer> order) {
+        if(firstIndex >= lastIndex) {
+            return mixedArray[];
+        }
+
+        int middleIndex = (lastIndex - firstIndex) / 2 + firstIndex;
+
+        T[] leftSubArr = execute(mixedArray, firstIndex, middleIndex, order);
+        T[] rightSubArr = execute(mixedArray, middleIndex + 1, lastIndex, order);
+
+        return merge(leftSubArr, rightSubArr, order);
+    }
+
+    private T[] merge(T[] leftSubArr, T[] rightSubArr, Predicate<Integer> order) {
 
     }
 }
